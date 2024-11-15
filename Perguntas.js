783b570fb0a7ad122632d300b3fd92aeacca7ee8
NOELADAVILA <00001090063519sp@al.educacao.sp.gov.br>
2024-11-14 21:01:32 -0300

    Add files via upload

---
+++ Perguntas.js
@@ -0,0 +1,54 @@
+criaCartao(
+    'Programação',
+    'O que é Python?',
+    'O Python é uma linguagem de programação'
+)
+
+criaCartao(
+    'Geografia',
+    'Qual a capital da França?',
+    'A capital da França é Paris'
+)
+
+criaCartao(
+    'Programação',
+    'O que é uma função?',
+    'Uma função é um bloco de código que executa alguma tarefa'
+)
+
+criaCartao(
+    'política',
+    'Qual o melhor presidente do Brasil?',
+    'Jair Bolsonaro'
+)
+
+criaCartao(
+    'Geografia',
+    'Qual a capital do Egito?',
+    'A capital do Egito écairo'
+)
+
+criaCartao(
+    'Matemática',
+    'quanto é 10% de 100?',
+    ' 10% de 100 é 10'
+)
+
+criaCartao(
+    'Matemática',
+    'Qual a formula de Bhaskhara?',
+    ' a formula de Bhaskhara é b±√(b²-4ac))/(2a)'
+)
+
+criaCartao(
+    'Historia',
+    'Em que ano terminou a Segunda Guerra Mundial?',
+    'Terminou em 2 de Setembro de 1945'
+)
+
+
+criaCartao(
+    'pessoal',
+    'O quão legal é o professor careca Márcio Mandalho?',
+    '100%'
+)
